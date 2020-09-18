58f63f0d2388ed1999cfefbbf3e28fc486560569
Dmytro Cisar <dmytro.cisar@gmail.com>
2020-09-18 17:09:21 +0300

    main file, v1.0.1

---
+++ index.js
@@ -0,0 +1,4 @@
+import BaseLayout from './src/components/base-layout/base-layout.vue';
+import BaseLayoutList from './src/components/base-layout-list/base-layout-list.vue';
+
+export { BaseLayout, BaseLayoutList };
