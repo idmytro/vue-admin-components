ce048e75a728886db710dc44c8c04e44607ea1ef
Dmytro Cisar <dmytro.cisar@gmail.com>
2020-09-18 15:21:56 +0300

    base-layout, .npmignore

---
+++ src/components/base-layout/base-layout.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="base-layout">
+    <div
+      v-if="$slots.menu"
+      class="base-layout__menu"
+    >
+      <slot name="menu"></slot>
+    </div>
+    <div class="base-layout__main">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style>
+.base-layout {
+  --width-sidemenu: 72px;
+
+  display: flex;
+  min-height: 100vh;
+}
+
+.base-layout__menu {
+  position: fixed;
+  left: 0;
+  top: 0;
+  bottom: 0;
+}
+
+.base-layout__main {
+  flex-grow: 1;
+  min-width: 0;
+}
+
+.base-layout__menu + .base-layout__main {
+  margin-left: var(--width-sidemenu);
+}
+</style>
