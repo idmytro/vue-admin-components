464f40484089a3ac903cea8d808a25282ed57fe3
Dmytro Cisar <dmytro.cisar@gmail.com>
2020-09-28 15:34:04 +0300

    v1.8.1, base-layout.js

---
+++ src/components/base-layout/base-layout.js
@@ -0,0 +1,18 @@
+
+export default {
+  template: `
+    <template>
+      <div class="base-layout">
+        <div
+          v-if="$slots.menu"
+          class="base-layout__menu"
+        >
+          <slot name="menu"></slot>
+        </div>
+        <div class="base-layout__main">
+          <slot></slot>
+        </div>
+      </div>
+    </template>
+  `,
+};
