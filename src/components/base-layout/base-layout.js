887bd059e80d175c8832ce610f21d56c8370d245
Dmytro Cisar <dmytro.cisar@gmail.com>
2020-09-28 16:46:41 +0300

    base-layout.js fix

--- src/components/base-layout/base-layout.js
+++ src/components/base-layout/base-layout.js
@@ -1,18 +1,16 @@
 
 export default {
   template: `
-    <template>
-      <div class="base-layout">
-        <div
-          v-if="$slots.menu"
-          class="base-layout__menu"
-        >
-          <slot name="menu"></slot>
-        </div>
-        <div class="base-layout__main">
-          <slot></slot>
-        </div>
+    <div class="base-layout">
+      <div
+        v-if="$slots.menu"
+        class="base-layout__menu"
+      >
+        <slot name="menu"></slot>
       </div>
-    </template>
+      <div class="base-layout__main">
+        <slot></slot>
+      </div>
+    </div>
   `,
 };
--- src/components/base-layout/base-layout.vue
+++ src/components/base-layout/base-layout.vue
@@ -12,27 +12,4 @@
   </div>
 </template>
 
-<style>
-.base-layout {
-  --width-sidemenu: 72px;
-
-  display: flex;
-  min-height: 100vh;
-}
-
-.base-layout__menu {
-  position: fixed;
-  left: 0;
-  top: 0;
-  bottom: 0;
-}
-
-.base-layout__main {
-  flex-grow: 1;
-  min-width: 0;
-}
-
-.base-layout__menu + .base-layout__main {
-  margin-left: var(--width-sidemenu);
-}
-</style>
+<style src="./base-layout.css"></style>
