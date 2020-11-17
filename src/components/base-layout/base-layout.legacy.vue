4663f96dd63ceda271be3c470ebd0cf975825d34
Dmytro Cisar <dmytro.cisar@gmail.com>
2020-11-17 16:21:06 +0200

    base-layout.legacy, v1.8.4

---
+++ src/components/base-layout/base-layout.legacy.vue
@@ -0,0 +1,24 @@
+<template id="BaseLayout">
+  <div class="base-layout">
+    <div
+      v-if="$slots.menu"
+      class="base-layout__menu"
+    >
+      <slot name="menu"></slot>
+    </div>
+    <div class="base-layout__main">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+((global) => {
+  global.components.BaseLayout = global.Vue.component(
+    'BaseLayout',
+    {
+      template: '#BaseLayout',
+    },
+  );
+})(window);
+</script>
