035a8b6f7f206113ce7f1093a43683ec074d76cd
Dmytro Cisar <dmytro.cisar@gmail.com>
2021-02-04 17:34:42 +0200

    + cell-class-name.mixin, v2.0.4

---
+++ src/mixins/cell-class-name.mixin.vue
@@ -0,0 +1,23 @@
+<script>
+((global) => {
+  global.mixins.cellClassName = {
+    computed: {
+      cellClassName () {
+        return ({ columnIndex }) => {
+          const count = this.columnCount;
+          if (!count) return '';
+
+          switch (columnIndex) {
+            case count - 1:
+              return 'last-column';
+            case count:
+              return 'gap-column';
+            default:
+              return '';
+          }
+        };
+      },
+    },
+  };
+})(window);
+</script>
