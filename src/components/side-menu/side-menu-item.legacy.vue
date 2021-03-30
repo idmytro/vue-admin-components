bb7584f47ac1033a042d124305dea8698075377c
Dmytro Cisar <dmytro.cisar@gmail.com>
2021-03-30 22:14:30 +0300

    side-menu*.legacy, update layout-list.css, v3.1.0

---
+++ src/components/side-menu/side-menu-item.legacy.vue
@@ -0,0 +1,113 @@
+<template id="side-menu-item">
+  <div
+    class="side-menu-item"
+    :class="type && `side-menu-item--${type}`"
+  >
+    <button
+      v-if="type === 'exit'"
+      type="submit"
+      :aria-label="tooltip || label"
+      class="side-menu-item__control"
+    >
+      <el-tooltip
+        :disabled="!tooltip"
+        :content="tooltip"
+        effect="dark"
+        placement="right"
+      >
+        <span
+          class="side-menu-item__icon"
+          v-html="svg"
+        ></span>
+      </el-tooltip>
+    </button>
+
+    <a
+      v-else-if="type === 'link' && !isDisabled"
+      :aria-label="tooltip || label"
+      :href="to"
+      target="_blank"
+      class="side-menu-item__control"
+    >
+      <el-tooltip
+        :disabled="!tooltip"
+        :content="tooltip"
+        effect="dark"
+        placement="right"
+      >
+        <span
+          class="side-menu-item__icon"
+          v-html="svg"
+        ></span>
+      </el-tooltip>
+    </a>
+
+    <router-link
+      v-else
+      :aria-label="tooltip || label"
+      :disabled="isDisabled"
+      :tag="computedTag"
+      :to="to"
+      class="side-menu-item__control"
+    >
+      <el-tooltip
+        :disabled="!tooltip"
+        :content="tooltip"
+        :tabindex="isDisabled ? -1 : 0"
+        effect="dark"
+        placement="right"
+      >
+        <span
+          class="side-menu-item__icon"
+          v-html="svg"
+        ></span>
+      </el-tooltip>
+    </router-link>
+  </div>
+</template>
+
+<script>
+((global) => {
+  global.components.SideMenu = Vue.component('SideMenu', {
+    template: '#side-menu-item',
+    props: {
+      disabled: {
+        type: Boolean,
+        default: false,
+      },
+      icon: {
+        type: String,
+        default: '',
+      },
+      label: {
+        type: String,
+        default: '',
+      },
+      to: {
+        type: [String, Object],
+        default: null,
+      },
+      tooltip: {
+        type: String,
+        default: '',
+      },
+      type: {
+        type: String,
+        default: '',
+      },
+    },
+    computed: {
+      isDisabled () {
+        return this.disabled || !this.to;
+      },
+      computedTag () {
+        return this.isDisabled ? 'button' : 'a';
+      },
+      svg () {
+        if (!this.icon) return '';
+        return `<svg><use xlink:href="#${this.icon}"></use></svg>`;
+      },
+    },
+  });
+})(window);
+</script>
---
+++ src/components/side-menu/side-menu.legacy.vue
@@ -0,0 +1,34 @@
+<template id="side-menu">
+  <div class="side-menu">
+    <side-menu-item
+      type="logo"
+      to="/"
+      label="Catalyst"
+    ></side-menu-item>
+
+    <div class="side-menu__list">
+      <slot></slot>
+    </div>
+
+    <form
+      action="/api/v1/logout"
+      method="post"
+    >
+      <side-menu-item
+        icon="side-menu-icon--exit"
+        tooltip="Выйти"
+        type="exit"
+      ></side-menu-item>
+    </form>
+
+    <side-menu-icons></side-menu-icons>
+  </div>
+</template>
+
+<script>
+((global) => {
+  global.components.SideMenu = Vue.component('SideMenu', {
+    template: '#side-menu',
+  });
+})(window);
+</script>
