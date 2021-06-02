0c7693d25314dddd1c291cdc9d18deb597785efc
Dmytro Cisar <dmytro.cisar@gmail.com>
2021-06-02 17:05:14 +0300

    + background-button, v3.4.0

---
+++ src/stories/button.stories.js
@@ -0,0 +1,33 @@
+import '../styles/element-ui/index.css';
+import '../styles/variables.css';
+import '../styles/base.css';
+import '../styles/buttons.css';
+import '../../storybook-example/button.css';
+
+export default {
+  title: 'Components/button',
+};
+
+export const Default = () => ({
+  template: `
+    <div style="padding: 20px">
+      <button type="button" class="secondary-button">Кнопка</button>
+    </div>
+  `,
+});
+
+export const DefaultOnWhite = () => ({
+  template: `
+    <div style="padding: 20px; background-color: #fff;">
+      <button type="button" class="secondary-button">Кнопка</button>
+    </div>
+  `,
+});
+
+export const BackgroundButton = () => ({
+  template: `
+    <div style="padding: 20px; background-color: #fff;">
+      <button type="button" class="background-button">Кнопка</button>
+    </div>
+  `,
+});
