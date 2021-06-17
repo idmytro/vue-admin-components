8e315a611cec2a128e5f6aaa99d7b6c0beaad758
Dmytro Cisar <dmytro.cisar@gmail.com>
2021-06-17 14:49:42 +0300

    windicss ^3.0.0, base-layout start mobile, v3.5.0

--- src/components/base-layout/base-layout.vue
+++ src/components/base-layout/base-layout.vue
@@ -2,7 +2,7 @@
   <div class="base-layout">
     <div
       v-if="$slots.menu"
-      class="base-layout__menu"
+      class="base-layout__menu z-11"
     >
       <slot name="menu"></slot>
     </div>
