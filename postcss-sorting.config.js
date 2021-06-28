1c9a834b8800a10bbb5c48aa8ebd391aeb7b7169
Dmytro Cisar <dmytro.cisar@gmail.com>
2021-06-28 16:44:36 +0300

    + postcss-sorting

---
+++ postcss-sorting.config.js
@@ -0,0 +1,17 @@
+module.exports = (ctx) => ({
+  plugins: {
+    'postcss-sorting': {
+      order: [
+        'custom-properties',
+        'dollar-variables',
+        'declarations',
+        'at-rules',
+        'rules',
+      ],
+
+      'properties-order': 'alphabetical',
+
+      'unspecified-properties-position': 'bottom',
+    },
+  },
+});
